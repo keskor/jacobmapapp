2c9bdcea71d5097df2161d70b10676aac99de581
keskor <35984529+keskor@users.noreply.github.com>
2018-03-06 10:32:58 -0700

    Added Gizmos

--- tethysapp-jacobmapapp/tethysapp/jacobmapapp/controllers.py
+++ tethysapp-jacobmapapp/tethysapp/jacobmapapp/controllers.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from tethys_sdk.gizmos import Button
+from tethys_sdk.gizmos import Button, ToggleSwitch, RangeSlider, DatePicker
 
 @login_required()
 def home(request):
     """
-    Controller for the app home page.
-    """
+        Controller for the app home page.
+        """
+    toggle_switch = ToggleSwitch(
+        display_text='Styled Toggle',
+        name='toggle2',
+        on_label='Yes',
+        off_label='No',
+        on_style='success',
+        off_style='danger',
+        initial=True,
+        size='large'
+    )
+
     save_button = Button(
         display_text='',
         name='save-button',
@@ -64,6 +75,7 @@ def home(request):
     )
 
     context = {
+        'toggle_switch': toggle_switch,
         'save_button': save_button,
         'edit_button': edit_button,
         'remove_button': remove_button,
@@ -77,9 +89,53 @@ def mapview(request):
     """
     map view page for my app
     """
+    toggle_switch_DEM = ToggleSwitch(
+        display_text='Utah DEM',
+        name='DEM_toggle',
+        on_label='Yes',
+        off_label='No',
+        on_style='success',
+        off_style='danger',
+        initial=True,
+        size='large'
+    )
+
+    toggle_switch_road = ToggleSwitch(
+        display_text='Roads',
+        name='road_toggle',
+        on_label='Yes',
+        off_label='No',
+        on_style='success',
+        off_style='danger',
+        initial=True,
+        size='large'
+    )
+
+    date_picker = DatePicker(
+        name='date1',
+        display_text='Prediction Date',
+        autoclose=True,
+        format='MM d, yyyy',
+        start_date='3/6/2018',
+        start_view='month',
+        today_button=True,
+        initial='March 6, 2018'
+    )
+
+    distance_slider = RangeSlider(
+        display_text='Distance of Interest (km)',
+        name='distance_slider',
+        min=0,
+        max=50,
+        initial=25,
+        step=5
+    )
+
     context={
-
-
+        'toggle_switch_DEM': toggle_switch_DEM,
+        'toggle_switch_road': toggle_switch_road,
+        'date_picker': date_picker,
+        'distance_slider': distance_slider
     }
 
     return render(request, 'jacobmapapp/mapview.html', context)
@@ -89,7 +145,6 @@ def about(request):
     about page for my app
     """
     context={
-
 
     }
 
